e91fead6f2c6811849c282f637d5469d4f57a399
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 15:55:12 +0900

    Fibonacci

---
+++ Programmers/Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(5);
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = Fibonacci(n);
+            return answer;
+        }
+
+        private int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int prev1 = 0;
+            int prev2 = 1;
+            int result = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = (prev1 + prev2) % 1234567;  // 나머지 연산 추가
+                prev1 = prev2;
+                prev2 = result;
+            }
+
+            return result;
+        }
+    }
+}
