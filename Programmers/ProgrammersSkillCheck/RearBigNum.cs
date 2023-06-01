29d42f6e02274c360ac19260c9df319b456c8983
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 13:23:09 +0900

    RearBigNum

---
+++ Programmers/ProgrammersSkillCheck/RearBigNum.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace ProgrammersSkillCheck
+{
+    internal class RearBigNum
+    {
+        public int[] solution(int[] numbers)
+        {
+            int[] answer = new int[numbers.Length];
+
+            for(int i = 0; i < numbers.Length; ++i)
+            {
+                int[] checkArr = numbers.Skip(i + 1).ToArray();
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ Programmers/RearBigNum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace RearBigNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] numbers = new int[] { 9, 1, 5, 3, 6, 2 };
+            solution.solution(numbers);
+        }
+    }
+
+
+    public class Solution
+    {
+        public int[] solution(int[] numbers)
+        {
+            int[] answer = new int[numbers.Length];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (stack.Count > 0 && numbers[stack.Peek()] < numbers[i])
+                {
+                    answer[stack.Pop()] = numbers[i];
+                }
+                stack.Push(i);
+            }
+
+            while (stack.Count > 0)
+            {
+                answer[stack.Pop()] = -1;
+            }
+
+            return answer;
+        }
+    }
+}
