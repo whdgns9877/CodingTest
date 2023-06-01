f232e3bf0186782108cb3defe535769eea39d754
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 15:37:05 +0900

    CutSortIndex

---
+++ Programmers/CutSortIndex/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutSortIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] array = { 1, 5, 2, 6, 3, 7, 4 };
+            int[,] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+            solution.solution(array, commands);
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] array, int[,] commands)
+        {
+            int[] answer = new int[commands.GetLength(0)];
+            for(int i = 0; i < commands.GetLength(0); ++i)
+            {
+                int startIndex = commands[i, 0];  // 자를 부분의 시작 인덱스
+                int endIndex = commands[i, 1];    // 자를 부분의 마지막 인덱스
+
+                int length = endIndex - startIndex + 1;  // 자를 부분의 길이
+
+                int[] slicedArray = new int[length];  // 자른 결과를 저장할 새로운 배열 생성
+
+                Array.Copy(array, startIndex - 1, slicedArray, 0, length);  // 배열 자르기
+                Array.Sort(slicedArray); // 배열 정렬
+                answer[i] = slicedArray[commands[i,2] - 1];
+            }
+            return answer;
+        }
+    }
+}
---
+++ Programmers/PhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string phoneNum = "01033334444";
+            solution.solution(phoneNum);
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string phone_number)
+        {
+            string answer = new string('*', phone_number.Length - 4) + phone_number.Substring(phone_number.Length - 4);
+            return answer;
+        }
+    }
+}
