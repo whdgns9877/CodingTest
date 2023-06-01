b97b48ee643a8069dadb22e9a72faf209f6a79bc
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 11:12:43 +0900

    AddDivisor

---
+++ Programmers/ProgrammersSkillCheck/Adddivisors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ProgrammersSkillCheck
+{
+    public class Adddivisors
+    {
+        public int solution(int left, int right)
+        {
+            int answer = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                int count = GetDivisorCount(i);
+
+                answer += (count % 2 == 0) ? i : -i;
+            }
+
+            return answer;
+        }
+
+        private int GetDivisorCount(int number)
+        {
+            int count = 0;
+            int sqrt = (int)Math.Sqrt(number);
+
+            for (int i = 1; i <= sqrt; i++)
+            {
+                if (number % i == 0)
+                {
+                    count += 2; // 짝수 약수 개수 카운트 (i와 number/i)
+                }
+            }
+
+            // 제곱수인 경우 중복 카운트 방지
+            if (sqrt * sqrt == number)
+            {
+                count--;
+            }
+
+            return count;
+        }
+
+    }
+}
