0d9f5a30abc8fbe6514dfeff9074409ec7dc5f0a
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 17:42:01 +0900

    IronSuit

---
+++ Programmers/IronSuit/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IronSuit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(6);
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            // 점프, 순간이동
+            // 점프는 이동할수있는데 answer양이 증가한다
+            // 순간이동은 지금까지 온거리 x2 할수있다
+
+            while (n > 0)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n -= 1;
+                    answer += 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
