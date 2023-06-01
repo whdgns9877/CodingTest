08ea985a52a683c0f11dd216bf419f45a6738eb5
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 11:13:09 +0900

    ExcludingSmallElements

---
+++ Programmers/ProgrammersSkillCheck/ExcludingSmallElements.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace ProgrammersSkillCheck
+{
+    internal class ExcludingSmallElements
+    {
+        public int[] solution(int[] arr)
+        {
+            int[] answer = new int[] { };
+
+            if (arr.Length <= 1) answer = new int[1] { -1 };
+            else
+            {
+                int min = arr.Min();
+                int[] newArray = arr.Where(x => x != min).ToArray();
+                answer = newArray;
+            }
+
+            return answer;
+        }
+    }
+}
