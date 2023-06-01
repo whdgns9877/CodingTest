d76bc698500863e6a9ab5ee79f6e18659a75c88d
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-01 17:56:44 +0900

    JadenCase

---
+++ Programmers/JadenCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace JadenCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "3people unFollowed me";
+            solution.solution(s);
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            string[] words = s.ToLower().Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    if (char.IsDigit(words[i][0]) && words[i].Length > 1)
+                    {
+                        words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+                    }
+                    else
+                    {
+                        words[i] = textInfo.ToTitleCase(words[i]);
+                    }
+                }
+            }
+
+            string answer = string.Join(" ", words);
+            return answer;
+        }
+    }
+}
