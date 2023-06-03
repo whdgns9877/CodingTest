cbef36d982a0e38d5ad36610f5b84e5e1590a5e7
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-04 01:54:12 +0900

    AddMissingNum

---
+++ Programmers/AddMissingNums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace AddMissingNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] numbers = { 1, 2, 3, 4, 6, 7, 8, 0 };
+            solution.solution(numbers);
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] numbers)
+        {
+            int answer = 0;
+            for (int i = 0; i <= 9; ++i)
+            {
+                if (!numbers.Contains(i))
+                {
+                    answer += i;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ Programmers/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int s = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < s; ++i)
+            {
+                for (int j = 0; j <= i; ++j)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
