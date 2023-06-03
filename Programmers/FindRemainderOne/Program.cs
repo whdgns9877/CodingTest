4e13d10ba21efc6484f57b969f83b3de0bc37784
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-03 20:24:02 +0900

    FindRemainderOne

---
+++ Programmers/FindRemainderOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindRemainderOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(3);
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for(int i = 1; i < n; ++i)
+            {
+                if(n % i == 1)
+                {
+                    answer = i;
+                    break;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
