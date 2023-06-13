279ab31f645d3c1b2ecb5ff589044a024f38bb5f
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-13 16:05:01 +0900

    AvantGardeTiling

---
+++ Programmers/AvantGardeTiling/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AvantGardeTiling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(4);
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int MOD = 1000000007;
+            long[] dp = new long[100001];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 3;
+            dp[3] = 10;
+            dp[4] = 23;
+            dp[5] = dp[4] + dp[3] * 2 + dp[2] * 5 + dp[1] * 2 + dp[0] * 2;
+
+            for (int i = 6; i <= n; i++)
+            {
+                dp[i] = (dp[i - 1] + (dp[i - 2] * 2) % MOD + (dp[i - 3] * 6) % MOD + dp[i - 4] % MOD - dp[i - 6] % MOD + MOD) % MOD;
+            }
+            return (int)(dp[n] % MOD);
+        }
+    }
+
+
+}
