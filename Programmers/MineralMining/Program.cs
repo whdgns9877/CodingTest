f69be7724410d1c7520fa8eb0da2c7a2ba68f836
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-17 20:47:02 +0900

    MineralMining

---
+++ Programmers/MineralMining/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineralMining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] picks, string[] minerals)
+        {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
