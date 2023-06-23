06087e9aa2d9b2bdfd4e9fa3a9baeb92b4ed564f
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-23 13:57:29 +0900

    ClosetSimilar

---
+++ Programmers/ClosestSimilarLetter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosestSimilarLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "banana";
+            solution.solution(s);
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(string s)
+        {
+            int[] answer = new int[s.Length];
+            int idx = 0;
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in s) 
+            {
+                if (dic.ContainsKey(c))
+                {
+                    answer[idx] = idx - dic[c];
+                    dic[c] = idx;
+                }
+                else
+                {
+                    dic.Add(c, idx);
+                    answer[idx] = -1;
+                }
+                idx++;
+            }
+
+            return answer;
+        }
+    }
+}
