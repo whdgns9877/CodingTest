cc6371298a6ff2914a5f7fec5affaab2cb20f32a
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-23 13:58:00 +0900

    PasswordForTwo

---
+++ Programmers/PasswordForTwo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace PasswordForTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "klmnopqrstuvwxyz";
+            string skip = "abcdefghij";
+            solution.solution(s, skip, 20);
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string s, string skip, int index)
+        {
+            string answer = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                for (int j = 0; j < index; j++)
+                {
+                    c += (char)1;
+                    if (c > 'z')
+                    {
+                        c -= (char)26;
+                    }
+                    if (skip.Contains(c))
+                    {
+                        j--;
+                    }
+                }
+                answer += c;
+            }
+
+            return answer;
+        }
+    }
+}
