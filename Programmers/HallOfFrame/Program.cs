59f6dcd9800588cf1cb3ca75d863f62c381dea65
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-23 22:40:59 +0900

    HallOfFrame

---
+++ Programmers/HallOfFrame/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HallOfFrame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] score = { 0, 300, 40, 300, 20, 70, 150, 50, 500, 1000 };
+            int[] answer = solution.solution(4, score);
+            foreach (var item in answer)
+            {
+                Console.Write(item + " "); 
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int k, int[] score)
+        {
+            List<int> list = new List<int>();
+            int[] answer = new int[score.Length];
+            int idx = 0;
+
+            foreach (int num in score)
+            {
+                if (idx < k)
+                {
+                    list.Add(num);
+                }
+                else if (list.Min() < num)
+                {
+                    list.Remove(list.Min());
+                    list.Add(num);
+                }
+                answer[idx] = list.Min();
+                idx++;
+            }
+
+            return answer;
+        }
+    }
+}
