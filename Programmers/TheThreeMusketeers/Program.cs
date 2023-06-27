0a5bb7ebd83e49abd424474ba58bc02156444874
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-27 14:24:52 +0900

    TheThreeMusketeers

---
+++ Programmers/TheThreeMusketeers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheThreeMusketeers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] number = { -2, 3, 0, 2, -5 };
+            Console.WriteLine(solution.solution(number));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] number)
+        {
+            int answer = 0;
+
+            var combinations = number
+            .SelectMany((x, i) => number.Skip(i + 1)
+                .SelectMany((y, j) => number.Skip(i + j + 2)
+                    .Select(z => new { X = x, Y = y, Z = z })))
+            .Where(triple => triple.X + triple.Y + triple.Z == 0);
+
+            answer = combinations.Count();
+
+            return answer;
+        }
+    }
+}
