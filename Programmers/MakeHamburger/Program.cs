09afeb0387bbb35ada4b9ed9ed6b4f408a4a9bdd
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-27 09:25:53 +0900

    MakeHamburger

---
+++ Programmers/MakeHamburger/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace MakeHamburger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] ingredient = { 2, 1, 1, 2, 3, 1, 2, 3, 1 };
+            Console.WriteLine(solution.solution(ingredient));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] ingredients)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+
+            foreach (int ingredient in ingredients)
+            {
+                sb.Append(ingredient);
+
+                if (sb.Length >= 4 && sb.ToString(sb.Length - 4, 4) == "1231")
+                {
+                    sb.Remove(sb.Length - 4, 4);
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
