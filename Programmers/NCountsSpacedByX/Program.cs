fd591d105c8fb9b138200ff02157a5d1b3d89f51
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-27 21:47:13 +0900

    NCountsSpacedByX

---
+++ Programmers/NCountsSpacedByX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NCountsSpacedByX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(2,5));
+        }
+    }
+
+    public class Solution
+    {
+        public long[] solution(int x, int n)
+        {
+            long[] answer = new long[n];
+            long num = 0;
+            for(int i = 0; i < n; i++)
+            {
+                num += (long)x;
+                answer[i] = num;
+            }
+            return answer;
+        }
+    }
+}
