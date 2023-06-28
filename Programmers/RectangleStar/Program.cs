a20037c5cee1666bf9e4f1de00384b56a56dbf17
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 15:33:33 +0900

    RectangleStar

---
+++ Programmers/RectangleStar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RectangleStar
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string[] s;
+            s = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+
+            for(int i = 0; i < b; i++) 
+            {
+                for(int j = 0; j < a; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
