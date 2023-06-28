b7e7bc56a937d991c2156efd928161a2b1fb1ec8
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 10:21:28 +0900

    MakeArrayUsingNaturalNum

---
+++ Programmers/MakeArrayUsingNaturalNum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MakeArrayUsingNaturalNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(12345));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(long n)
+        {
+            char[] nToCharArr = n.ToString().ToCharArray();
+            Array.Reverse(nToCharArr);
+            int[] answer = new int[nToCharArr.Length];
+
+            for(int i = 0; i < nToCharArr.Length; i++)
+            {
+                answer[i] = int.Parse(nToCharArr[i].ToString());
+            }
+
+            return answer;
+        }
+    }
+}
