2209767db874f316e0f5ab99a1e1d5f86bad663c
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 12:47:06 +0900

    HidePhoneNumber

---
+++ Programmers/HidePhoneNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace HidePhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("01033334444"));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string phone_number)
+        {
+            StringBuilder sb = new StringBuilder();
+            int length = phone_number.Length;
+            for(int i = 0; i < phone_number.Length - 4; i++)
+            {
+                sb.Append("*");
+            }
+
+            sb.Append(phone_number[length - 4]);
+            sb.Append(phone_number[length - 3]);
+            sb.Append(phone_number[length - 2]);
+            sb.Append(phone_number[length - 1]);
+
+            return sb.ToString();
+        }
+    }
+}
