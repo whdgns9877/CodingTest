ac5c4f71698f7c83d0cb9e2e87146d220e92be32
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 10:50:38 +0900

    HarshadNumber

---
+++ Programmers/HarshadNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HarshadNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(12));
+        }
+    }
+
+    public class Solution
+    {
+        public bool solution(int x)
+        {
+            char[] xToCharArr = x.ToString().ToCharArray();
+
+            int sum = 0;
+            for(int i = 0; i < xToCharArr.Length; i++)
+            {
+                sum += int.Parse(xToCharArr[i].ToString());
+            }
+
+            return x % sum == 0 ? true : false;
+        }
+    }
+}
