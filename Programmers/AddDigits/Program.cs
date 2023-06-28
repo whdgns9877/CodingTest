98a56f86112a249da23d35a5bdbbb226017698bc
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 09:49:50 +0900

    AddDigits

---
+++ Programmers/AddDigits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AddDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(987));
+        }
+    }
+
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            string s = n.ToString();
+            int answer = 0;
+
+            foreach (char digit in s)
+            {
+                answer += int.Parse(digit.ToString());
+            }
+
+            return answer;
+        }
+    }
+}
