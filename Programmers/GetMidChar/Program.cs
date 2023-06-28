347b40c0a171bbc5dbb11c27edcaf9743bcf7785
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 13:01:59 +0900

    GetMidChar

---
+++ Programmers/GetMidChar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GetMidChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("abcde"));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            int middle = s.Length / 2; 
+            if (s.Length % 2 == 0)
+            {
+                return s[middle - 1].ToString() + s[middle].ToString();
+            }
+            else
+            {
+                return s[middle].ToString();
+            }
+        }
+    }
+}
