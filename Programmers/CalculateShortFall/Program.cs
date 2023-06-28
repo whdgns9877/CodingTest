1a8bb8d006a9f02f4e4b2cd10058f7626c5d5e66
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 14:30:36 +0900

    CalculateShortFall

---
+++ Programmers/CalculateShortFall/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculateShortFall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(3, 20, 4));
+        }
+    }
+
+    class Solution
+    {
+        public long solution(int price, int money, int count)
+        {
+            long curUseMoney = 0;
+
+            for(int i = 1; i <= count; i++)
+            {
+                curUseMoney += price * i;
+            }
+
+            return curUseMoney - money < 0 ? 0 : curUseMoney - money;
+        }
+    }
+}
