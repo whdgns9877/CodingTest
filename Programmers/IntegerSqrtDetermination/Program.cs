5aeddb1f033d52bea30976a83a37d64f53c94488
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 10:21:46 +0900

    IntegerSqrtDetermination

---
+++ Programmers/IntegerSqrtDetermination/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntegerSqrtDetermination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(121));
+        }
+    }
+
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            long sqrt =  (int)Math.Sqrt(n);
+            return sqrt * sqrt == n ? (sqrt + 1) * (sqrt + 1) : -1;
+        }
+    }
+}
