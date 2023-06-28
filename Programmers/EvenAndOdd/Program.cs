40ce24b2932eaf889123baa7ac5cf77a64a5c90c
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 09:49:44 +0900

    EvenAndOdd

---
+++ Programmers/EvenAndOdd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenAndOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(3));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            return num % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
