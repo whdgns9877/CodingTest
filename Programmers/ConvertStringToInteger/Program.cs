0dec3f04e1fd0884b155894d116c6fb861773c48
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 10:25:58 +0900

    ConvertStringToInteger

---
+++ Programmers/ConvertStringToInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConvertStringToInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("1234"));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(string s)
+        {
+            return int.Parse(s);
+        }
+    }
+}
