f4bcfd88f71cee520cebdee8776f020ae3dda489
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 11:37:02 +0900

    FindKimInSeoul

---
+++ Programmers/FindKimInSeoul/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FindKimInSeoul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string[] seoul = { "Jane", "Kim" };
+            Console.WriteLine(solution.solution(seoul));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string[] seoul)
+        {
+            const string KIM = "Kim";
+            int index = 0;
+            for (int i = 0; i < seoul.Length; i++)
+            {
+                if (seoul[i].Equals(KIM))
+                {
+                    index = i;
+                }
+            }
+
+            return $"김서방은 {index}에 있다";
+        }
+    }
+}
