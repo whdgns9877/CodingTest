352b0e9472b2dc744751fae5b5edf0e8a2417cc8
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 14:09:50 +0900

    StringArrangeDescending

---
+++ Programmers/StringArrangeDescending/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringArrangeDescending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("Zbcdefg"));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            char[] chars = s.ToCharArray();
+            Array.Sort(chars, (x,y) => y.CompareTo(x));
+            return new string(chars);
+        }
+    }
+}
