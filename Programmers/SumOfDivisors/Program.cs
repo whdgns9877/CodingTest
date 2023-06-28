bbd5dd5081bea4cda61c70469d92ba38ef2285a3
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 09:49:25 +0900

    SumOfDivisors

---
+++ Programmers/SumOfDivisors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDivisors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(12));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
