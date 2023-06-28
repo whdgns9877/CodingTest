64e979c7482cd56bce51a197d08f34e5208542bf
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 12:36:04 +0900

    SignPlus

---
+++ Programmers/SignPlus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace SignPlus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] absolutes = { 4, 7, 12 };
+            bool[] signs = { true, false, true };
+            Console.WriteLine(solution.solution(absolutes, signs));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] absolutes, bool[] signs)
+        {
+            for(int i = 0; i < absolutes.Length; i++)
+            {
+                if (signs[i] == false)
+                {
+                    absolutes[i] *= -1;
+                }
+            }
+
+            return absolutes.Sum();
+        }
+    }
+}
