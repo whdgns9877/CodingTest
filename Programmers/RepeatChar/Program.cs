8194bc48e11cb7e3c4922b900a191d0793db22ab
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 13:10:35 +0900

    RepeatChar

---
+++ Programmers/RepeatChar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(7));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(int n)
+        {
+            const string SU = "수";
+            const string BAK = "박";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(i % 2 == 0 ? SU : BAK);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
