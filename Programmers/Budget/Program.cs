669104e93a07b07292e84d5d46b0a8766d677c08
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 20:59:23 +0900

    namespace Budget

---
+++ Programmers/Budget/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Budget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] d = { 1, 3, 2, 5, 4 };
+            Console.WriteLine(solution.solution(d, 9));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] d, int budget)
+        {
+            int answer = 0;
+            int curSum = 0;
+            List<int> dList = new List<int>(d);
+            dList.Sort();
+
+            for(int i = 0; i < dList.Count; i++)
+            {
+                if(curSum + dList[i] > budget)
+                {
+                    break;
+                }
+                curSum += dList[i];
+                answer ++;
+            }
+
+            return answer;
+        }
+    }
+}
