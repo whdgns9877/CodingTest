60cafbba267d6f46670615afc7912813cb9f68e0
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 13:15:05 +0900

    DotProduct

---
+++ Programmers/DotProduct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DotProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] a = { 1, 2, 3, 4 };
+            int[] b = { -3, -1, 0, 2 };
+            Console.WriteLine(solution.solution(a, b));
+        }
+    }
+
+
+    public class Solution
+    {
+        public int solution(int[] a, int[] b)
+        {
+            int answer = 0;
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                answer += a[i] * b[i];
+            }
+
+            return answer;
+        }
+    }
+}
