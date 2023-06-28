9c0e52622d7f305d1fde40983af7036381c4716e
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 11:26:34 +0900

    CollatzGuess

---
+++ Programmers/CollatzGuess/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CollatzGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(626331));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int num)
+        {
+            if (num == 1) return 0;
+            int count = 0;
+            long curNum = num;
+            while (count < 500)
+            {
+                if (curNum % 2 == 0)
+                {
+                    curNum /= 2;
+                }
+                else
+                {
+                    curNum = (curNum * 3) + 1;
+                }
+                count++;
+                if (curNum == 1) return count;
+            }
+
+            return -1;
+        }
+    }
+}
