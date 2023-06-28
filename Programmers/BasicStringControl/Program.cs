7430d9b0a03a9aafa402b06501b69145300d645f
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 14:38:10 +0900

    BasicStringControl

---
+++ Programmers/BasicStringControl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BasicStringControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("121212"));
+        }
+    }
+
+    public class Solution
+    {
+        public bool solution(string s)
+        {
+            if(s.Length == 4 ||  s.Length == 6)
+            {
+                int result = int.MaxValue;
+                return int.TryParse(s, out result);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
