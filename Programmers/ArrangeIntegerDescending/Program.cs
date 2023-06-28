8aaf35a293035ab36d9c395c29d8641d2681cbc0
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 10:35:36 +0900

    ArrangeIntegerDescending

---
+++ Programmers/ArrangeIntegerDescending/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace ArrangeIntegerDescending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(118372));
+        }
+    }
+
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            char[] nToCharArr = n.ToString().ToCharArray();
+            Array.Sort(nToCharArr, (x,y) => y.CompareTo(x));
+            
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in nToCharArr) 
+            {
+                sb.Append(c);
+            }
+
+            long answer = long.Parse(sb.ToString());
+            return answer;
+        }
+    }
+}
