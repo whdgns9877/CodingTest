e629d106db22ec5bee703291ac7c2361909bccd8
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 12:54:21 +0900

    RemoveMinNumber

---
+++ Programmers/RemoveMinNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveMinNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] arr = { 4, 3, 2, 1 };
+            Console.WriteLine(solution.solution(arr));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            List<int> list = new List<int>(arr);
+            list.Remove(list.Min());
+
+            if(list.Count == 0) list.Add(-1);
+
+            return list.ToArray();
+        }
+    }
+}
