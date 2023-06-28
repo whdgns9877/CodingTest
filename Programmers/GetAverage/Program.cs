272cffb0af6dfd4d709548e42f40f7fa00801f8d
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 09:55:46 +0900

    GetAverage

---
+++ Programmers/GetAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace GetAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] arr = { 1, 2, 3, 4 };
+            Console.WriteLine(solution.solution(arr));
+        }
+    }
+
+    public class Solution
+    {
+        public double solution(int[] arr)
+        {
+            return arr.Average();
+        }
+    }
+}
