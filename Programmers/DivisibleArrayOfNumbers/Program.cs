2146198173a69fcf59ce195cf9c0678f92f13706
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-28 11:49:19 +0900

    DivisibleArrayOfNumbers

---
+++ Programmers/DivisibleArrayOfNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DivisibleArrayOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] arr = { 5, 9, 7, 10 };
+            Console.WriteLine(solution.solution(arr, 5));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] arr, int divisor)
+        {
+            List<int> arrList = new List<int>();
+            Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % divisor == 0) arrList.Add(arr[i]);
+            }
+            if (arrList.Count == 0)
+            {
+                arrList.Add(-1);
+            }
+            return arrList.ToArray();
+        }
+    }
+}
