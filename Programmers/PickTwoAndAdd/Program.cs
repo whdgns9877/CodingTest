80a615ec9d24c0ddb420467f34109245e01872d7
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-29 12:47:17 +0900

    PickTwoAndAdd

---
+++ Programmers/PickTwoAndAdd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PickTwoAndAdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] numbers = { 2, 1, 3, 4, 1 };
+            Console.WriteLine(solution.solution(numbers));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] numbers)
+        {
+            List<int> sumList = new List<int>();
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                for(int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    if (!sumList.Contains(numbers[i] + numbers[j]))
+                    {
+                        sumList.Add(numbers[i] + numbers[j]);
+                    }
+                }
+            }
+
+            sumList.Sort();
+
+            return sumList.ToArray();
+        }
+    }
+}
