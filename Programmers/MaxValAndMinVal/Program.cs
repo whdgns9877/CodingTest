01dceab5b70257d15203cd65e2e8fd40655fcdd3
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-06-30 00:37:26 +0900

    MaxValAndMinVal

---
+++ Programmers/MaxValAndMinVal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxValAndMinVal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "1 2 3 4";
+            Console.WriteLine(solution.solution(s));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string s)
+        {
+            string[] numStrs = s.Split(' ');
+            int[] nums = Array.ConvertAll(numStrs, Convert.ToInt32);
+            string answer =  $"{nums.Min()} {nums.Max()}";
+            return answer;
+        }
+    }
+}
