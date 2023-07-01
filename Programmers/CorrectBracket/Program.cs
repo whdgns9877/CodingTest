90d4af67a8f85b79458defe2925b3cfeca06647f
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-01 22:59:37 +0900

    CorrectBracket

---
+++ Programmers/CorrectBracket/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CorrectBracket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "())(()";
+            Console.WriteLine(solution.solution(s));
+        }
+    }
+
+    public class Solution
+    {
+        public bool solution(string s)
+        {
+            // 닫힌채로 시작하면 맞출수 없다 || 인덱스 개수가 홀수라면 짝을 맞출수 없다
+            if (s[0] != '(' || s.Length % 2 == 1) 
+            {
+                return false;
+            }
+
+            int leftCount = 0;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                // '(' 모양이 나오고나서 몇개가 나오는지 세놓는다
+                if (s[i].Equals('('))
+                {
+                    leftCount++;
+                }
+                // ')' 모양이 나오고나서
+                else
+                {
+                    // 그 뒤 엔덱스들이 leftCount 만큼 ')' 모양인지 확인한다
+                    for(int j = leftCount; j < i + leftCount; j++)
+                    {
+                        // 중간에 '(' 모양이 들어가있다면 짝은 맞지않는다
+                        if(s[j].Equals('('))
+                        {
+                            return false;
+                        }
+                    }
+                    i += leftCount + 1;
+                    leftCount = 0;
+                }
+            }
+
+            return true;
+        }
+    }
+}
