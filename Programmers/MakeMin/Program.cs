6b3d029092a33482a94f85c0c7f19f40b9693bf7
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-02 21:34:15 +0900

    MakeMin

---
+++ Programmers/MakeMin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MakeMin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] A = { 1, 4, 2 };
+            int[] B = { 5, 4, 4 };
+            Console.WriteLine(solution.solution(A, B));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] A, int[] B)
+        {
+            int answer = 0;
+            Array.Sort(A);
+            Array.Sort(B, (x,y) => y.CompareTo(x));
+
+            int length = A.Length;
+
+            for(int i = 0; i < length; i++)
+            {
+                answer += A[i] * B[i];
+            }
+
+            return answer;
+        }
+    }
+}
