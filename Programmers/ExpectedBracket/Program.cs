8e8363cdf94db3455ee0f83edb8dc0cd71d5cec2
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-03 17:10:43 +0900

    ExpectedBracket

---
+++ Programmers/ExpectedBracket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExpectedBracket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(4, 1, 2));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n, int a, int b)
+        {
+            int answer = 0;
+            a = a - 1;
+            b = b - 1;
+            while (a / 2 != b / 2)
+            {
+                a = a / 2;
+                b = b / 2;
+                answer += 1;
+            }
+
+            answer += 1;
+            return answer;
+        }
+    }
+}
