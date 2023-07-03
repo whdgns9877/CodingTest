42cb4248087166f2f8d3b0fa7e07300076f8b713
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-03 10:58:05 +0900

    NextBigNum

---
+++ Programmers/NextBigNum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace NextBigNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(78));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n)
+        {
+            string numStr = Convert.ToString(n, 2);
+
+            int oneNum = Array.FindAll(numStr.ToCharArray(), x => x == '1').Count();
+
+            int nextOneNum = 0;
+
+            int idx = 1;
+            
+            while(true)
+            {
+                string nextNumStr = Convert.ToString(n + idx, 2);
+                nextOneNum = Array.FindAll(nextNumStr.ToCharArray(), x => x == '1').Count();
+                if(nextOneNum == oneNum)
+                {
+                    break;
+                }
+                idx++;
+            }
+
+            return n + idx;
+        }
+    }
+}
