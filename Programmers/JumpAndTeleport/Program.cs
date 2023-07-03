6201da6b25a5c0d825c2ea9ff1a1ae0bef0d8eb3
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-03 17:22:14 +0900

    JumpAndTeleport

---
+++ Programmers/JumpAndTeleport/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace JumpAndTeleport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(5000));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 1;
+
+            while(n != 1)
+            {
+                if(n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n--;
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
