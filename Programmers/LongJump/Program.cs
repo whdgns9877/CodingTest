d25d628885f79ccfe2df424b89741beb5acd9842
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-04 13:44:20 +0900

    LongJump

---
+++ Programmers/LongJump/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LongJump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(4));
+        }
+    }
+
+    public class Solution
+    {
+        public long solution(int n)
+        {
+            long[] dp = new long[2001];
+
+            dp[1] = 1;
+            dp[2] = 2;
+
+            if (n  == 1)
+            {
+                return dp[1];
+            }
+
+            if (n == 1)
+            {
+                return dp[2];
+            }
+            
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+            }
+            return dp[n];
+        }
+    }
+}
