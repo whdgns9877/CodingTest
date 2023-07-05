9e771b9b8b5c33f5a298fb876f955128ff5898b5
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-05 18:52:44 +0900

    ChooseMandarin

---
+++ Programmers/ChooseMandarin/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseMandarin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] tangerine = { 1, 3, 2, 5, 4, 5, 2, 3 };
+            Console.WriteLine(solution.solution(6, tangerine));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int k, int[] tangerine)
+        {
+            int answer = 0;
+            Dictionary<int, int> tanDic = new Dictionary<int, int>();
+
+            foreach (int i in tangerine) 
+            {
+                if (!tanDic.ContainsKey(i))
+                {
+                    tanDic.Add(i, 1);
+                }
+                else
+                {
+                    tanDic[i] += 1;
+                }
+            }
+
+            List<int> tangerineNums = new List<int>(tanDic.Values);
+            tangerineNums.Sort();
+            int curTangerine = 0;
+
+            while(k > curTangerine)
+            {
+                curTangerine += tangerineNums.Last();
+                tangerineNums.RemoveAt(tangerineNums.Count - 1);
+                answer++;
+            }
+
+            return answer;
+        }
+    }
+}
