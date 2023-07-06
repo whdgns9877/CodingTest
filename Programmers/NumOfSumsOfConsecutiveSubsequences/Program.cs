c2c386fb337271449556af841be8ca8f8cb320d7
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-06 15:19:47 +0900

    NumOfSumsOfConsecutiveSubsequences

---
+++ Programmers/NumOfSumsOfConsecutiveSubsequences/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumOfSumsOfConsecutiveSubsequences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] elements = { 7, 9, 1, 1, 4 };
+            Console.WriteLine(solution.solution(elements));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] elements)
+        {
+            HashSet<int> sumSet = new HashSet<int>();
+
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                int sum = 0;
+
+                for (int start = 0; start < elements.Length; start++)
+                {
+                    int end = (start + i) % elements.Length;
+                    sum += elements[end];
+                    sumSet.Add(sum);
+                }
+            }
+
+            return sumSet.Count;
+        }
+    }
+
+
+}
