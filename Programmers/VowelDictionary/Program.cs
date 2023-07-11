e20e40db8200a66132b819d355b261013668dac7
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-11 19:02:02 +0900

    VowelDictionary

---
+++ Programmers/VowelDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VowelDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string word = "I";
+            Console.WriteLine(solution.solution(word));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(string word)
+        {
+            int answer = 0;
+            int[] li = { 781, 156, 31, 6, 1 };
+
+            char[] vowels = { 'A', 'E', 'I', 'O', 'U' };
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                answer += li[i] * (Array.IndexOf(vowels, word[i]));
+            }
+
+            answer += word.Length;
+
+            return answer;
+        }
+    }
+}
