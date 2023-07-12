0c3aeb156858fb5146c306124ed66d4e91d04e20
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-12 21:26:27 +0900

    DifferentBitsUnderTwo

---
+++ Programmers/DifferentBitsUnderTwo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DifferentBitsUnderTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            long[] numbers = { 2, 7 };
+            Console.WriteLine(solution.solution(numbers));
+        }
+    }
+
+    public class Solution
+    {
+        public long[] solution(long[] numbers)
+        {
+            long[] answer = new long[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                long curNum = numbers[i];
+                long curNumPlusOne = curNum + 1;
+
+                while (CountDifferentBits(curNum, curNumPlusOne) > 2)
+                {
+                    curNumPlusOne++;
+                }
+
+                answer[i] = curNumPlusOne;
+            }
+
+            return answer;
+        }
+
+        private int CountDifferentBits(long num1, long num2)
+        {
+            long xorResult = num1 ^ num2;
+            int diffCount = 0;
+
+            while (xorResult > 0)
+            {
+                diffCount += (int)(xorResult & 1);
+                xorResult >>= 1;
+            }
+
+            return diffCount;
+        }
+    }
+}
